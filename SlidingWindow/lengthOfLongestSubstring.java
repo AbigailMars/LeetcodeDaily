3ab53f7891b83dc81cd5cc8136c504f6f01dbdbd
AbiagilMars <chenkunz@yahoo.com>
2021-05-10 15:31:14 +0200

    3. Longest Substring Without Repeating Characters

---
+++ SlidingWindow/lengthOfLongestSubstring.java
@@ -0,0 +1,22 @@
+
+public class lengthOfLongestSubstring {
+	   public int lengthOfLongestSubstring(String s) {
+	        int start = 0;
+	        int end = 0;
+	        int maxLength = 0;
+	        HashSet record = new HashSet();
+	        while(end < s.length()){
+	            if(record.add(s.charAt(end))){
+	                end++;
+	            }else{
+	                record.remove(s.charAt(start++));
+	            }
+	            if(maxLength < end - start){
+	                maxLength = end - start;
+	            }
+	        }
+	        return maxLength;
+	    }
+	   //Runtime: 6 ms, faster than 59.46% of Java online submissions for Longest Substring Without Repeating Characters.
+	   //Memory Usage: 39.1 MB, less than 49.81% of Java online submissions for Longest Substring Without Repeating Characters.
+}
